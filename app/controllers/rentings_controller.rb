14a2f191a7dbeab91041cd3d5f941d03f8b899b3
Abdelkrim Kiour <akiour@gmail.com>
2020-08-18 15:15:06 +0200

    renting controller and show renting

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,2 +1,24 @@
 class RentingsController < ApplicationController
+  def new
+    @renting = Renting.new
+    authorize @renting
+  end
+
+  def create
+    @renting = Renting.new(renting_params)
+    @renting.user = current_user
+    authorize @renting
+    @renting.save
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_renting
+    @renting = Renting.find(params[:id])
+  end
+
+    # Only allow a list of trusted parameters through.
+  def renting_params
+    params.require(:renting).permit(:id)
+  end
 end
--- app/policies/board_game_policy.rb
+++ app/policies/board_game_policy.rb
@@ -5,25 +5,25 @@ class BoardGamePolicy < ApplicationPolicy
     end
   end
 
-    def show?
-      return true
-    end
+  def show?
+    true
+  end
 
-    def create?
-      return true
-    end
+  def create?
+    true
+  end
 
-    def update?
-      user_is_owner_or_admin?
-    end
+  def update?
+    user_is_owner_or_admin?
+  end
 
-    def destroy?
-      user_is_owner_or_admin?
-    end
+  def destroy?
+    user_is_owner_or_admin?
+  end
 
-    private
+  private
 
-    def user_is_owner_or_admin?
-      @record.user == @user || @user.admin
-    end
+  def user_is_owner_or_admin?
+    @record.user == @user || @user.admin
+  end
 end
