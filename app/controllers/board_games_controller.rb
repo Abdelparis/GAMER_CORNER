b4aaab71bc5ded5567899cdf031f19818dd6c072
Abdelkrim Kiour <akiour@gmail.com>
2020-08-18 10:51:21 +0200

    cleaning branch

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,2 +1,6 @@
 class BoardGamesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  def index
+    @board_games = BoardGame.all
+  end
 end
--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -1,4 +1,5 @@
 class BoardGame < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  validates :name, :address, presence: :true
 end
