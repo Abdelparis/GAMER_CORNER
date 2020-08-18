c168b85d1bf5b707b24fa527a956f946d6bff699
Maelys Bassilekin Blanc <maelysbassilekinblanc@gmail.com>
2020-08-18 10:43:45 +0200

    changes searchbar

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,2 +1,12 @@
 class BoardGamesController < ApplicationController
+  def index
+    @params = params[:search]
+    if !@params.present?
+      @board_games = BoardGame.all
+    elsif @params[:name] && @params[:address] && @params[:starting_at] && @params[:expires_at]
+      @board_games = BoardGame.where("name ILIKE ?", "%#{@params[:name]}%")
+    else
+      @board_games = BoardGame.where("name ILIKE ?", "%#{@params[:name]}%")
+    end
+  end
 end
