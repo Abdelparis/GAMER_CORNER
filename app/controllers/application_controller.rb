15fe54595a3027298e52a3c0799b507402f1d543
Walid Tairi <tairiwalide@hotmail.fr>
2020-08-20 14:41:08 +0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -14,7 +13,6 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
-
   private
 
   def skip_pundit?
