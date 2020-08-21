a3368165634ce20bbd94299a693e0e311893baee
Abdelkrim Kiour <akiour@gmail.com>
2020-08-21 11:41:20 +0200

    update final dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
   get '/update-accepted/:id', to: 'rentings#update_accepted', as: 'accept'
-  get '/update-accepted/:id', to: 'rentings#update_rejected', as: 'reject'
+  get '/update-rejected/:id', to: 'rentings#update_rejected', as: 'reject'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :board_games, only: [:index, :show, :new, :create] do
     resources :rentings, only: [:new, :create]
